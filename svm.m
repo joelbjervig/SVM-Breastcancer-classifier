7e39985dfceb3fc4ca1701ea2b5d856ec4543826
Joel Bjervig <joelfbjervig@gmail.com>
2020-12-01 10:26:54 +0100

    Update svm.m

--- svm.m
+++ svm.m
@@ -1,8 +1,9 @@
 T = readtable('wdbc.dat');
-BorM = zeros(height(T),1);
+BorM = zeros(height(T),1); % Benign or Malignent
+
+% 1. Define auxiliary matrices for general nonseperable
 
 for i=1:length(BorM)
-    
     
     if T{i,2}=="M"
         BorM(i)=1;
